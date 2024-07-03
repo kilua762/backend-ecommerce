d8a85d473f99147a68b67f9a31b48b687bf066cc
kilua762 <53900196+kilua762@users.noreply.github.com>
2024-07-03 18:24:42 +0700

    lanjut rumah
    mulai frontemd

--- routes/api.php
+++ routes/api.php
@@ -91,7 +91,34 @@ Route::prefix('web')->group(function () {
     //sliders route
     Route::get('/sliders', [App\Http\Controllers\Api\Web\SliderController::class, 'index'], ['as' => 'web']);
 
+    //rajaongkir
+    Route::get('/rajaongkir/provinces', [App\Http\Controllers\Api\Web\RajaOngkirController::class, 'getProvinces'], ['as' => 'web']);
+
+    Route::post('/rajaongkir/cities', [App\Http\Controllers\Api\Web\RajaOngkirController::class, 'getCities'], ['as' => 'web']);
+
+    Route::post('/rajaongkir/checkOngkir', [App\Http\Controllers\Api\Web\RajaOngkirController::class, 'checkOngkir'], ['as' => 'web']);
+   
+    //get cart 
+    Route::get('/carts', [App\Http\Controllers\Api\Web\CartController::class, 'index'], ['as' => 'web'])->middleware('auth:api_customer');
+
+    //store cart
+    Route::post('/carts', [App\Http\Controllers\Api\Web\CartController::class, 'store'], ['as' => 'web'])->middleware('auth:api_customer');
     
+    //get cart price
+    Route::get('/carts/total_price', [App\Http\Controllers\Api\Web\CartController::class, 'getCartPrice'], ['as' => 'web'])->middleware('auth:api_customer');
+
+    //get cart weight
+    Route::get('/carts/total_weight', [App\Http\Controllers\Api\Web\CartController::class, 'getCartWeight'], ['as' => 'web'])->middleware('auth:api_customer');
+
+    //remove cart
+    Route::post('/carts/remove', [App\Http\Controllers\Api\Web\CartController::class, 'removeCart'], ['as' => 'web'])->middleware('auth:api_customer');
+
+    //checkout route
+    Route::post('/checkout', [App\Http\Controllers\Api\Web\CheckoutController::class, 'store'], ['as' => 'web'])->middleware('auth:api_customer');
+
+    //notification handler route
+    Route::post('/notification', [App\Http\Controllers\Api\Web\NotificationHandlerController::class, 'index'], ['as' => 'web']);
+
 });
 
 
